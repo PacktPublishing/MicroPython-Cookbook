1c2e58f03eddeda94a249389cbba48fd876de43e
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2019-02-22 21:22:33 +0300

    removed old chapter 01

--- Chapter 01/01_first.py
+++
@@ -1,5 +0,0 @@
-from adafruit_circuitplayground.express import cpx
-import time
-
-cpx.pixels[0] = (255, 0, 0)  # set first NeoPixel to the color red
-time.sleep(60)
--- Chapter 01/02_repl.py
+++
@@ -1,8 +0,0 @@
-def add(a, b):
-    return a + b
-
-
-add(2, 2)
-
-
-2**100 + 2**101
